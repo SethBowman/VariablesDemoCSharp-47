afb900a8972cfc638d5c318d28a8ed7ca0eaed8b
Seth Bowman <brandonsethbowman@gmail.com>
2024-04-26 13:49:04 -0500

    Add project files.

---
+++ VariablesDemoCSharp-47/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace VariablesDemoCSharp_47
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+
+            //Number types
+            //Declaring a variable (create a variable)
+            //Camel case - everyNextWordStartsWithCaptital
+            int wholeNumber;
+
+            //Initializing a variable (giving it a value)
+            wholeNumber = 5;
+
+            //Or do both
+            //Declaring and initializing
+            int otherNumber = 10;
+
+            otherNumber = 40;
+
+            double average = 99.99;
+
+            decimal myWallet = 500.00m;
+
+            //Word types
+
+            string ourClass = "CSharp-47";
+
+            char key = '1';
+
+            //Fact type
+
+            bool isCoding = true;
+
+            //String interpolation
+            //string message = $"Our class is {ourClass}";
+
+            //Console.WriteLine($"Our class is {ourClass}");
+
+            //Take user input
+
+            Console.WriteLine("Give me your name:");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {userName}");
+
+            
+
+        }
+    }
+}
